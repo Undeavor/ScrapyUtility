1047ca923de65b95c3cc3b21a2a74c5915d710d4
Undeavor <92637959+Undeavor@users.noreply.github.com>
2025-08-25 20:12:26 +0200

    Add files via upload

---
+++ berserk2/berserk2/spiders/berserk2spider.py
@@ -0,0 +1,19 @@
+import scrapy
+
+
+class Berserk2spiderSpider(scrapy.Spider):
+    name = 'berserk2spider'
+    allowed_domains = ['s22.anime-sama.fr']
+    start_urls = ['https://s22.anime-sama.fr/s1/scans/Berserk/1/']
+
+    def parse(self, response):
+        for i in range(9):
+            if i!=0:
+                tome_url = "https://s22.anime-sama.fr/s1/scans/Berserk/" + str(i) + "/"
+                yield response.follow(tome_url) 
+                for image_url in response.css('a::attr(href)').getall(): #magnifique code
+                    yield {'image_urls': [response.urljoin(image_url)]}  #magnifique code
+            else:
+                pass
+
+
